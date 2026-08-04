347f38d4472127ce953cb0f896c7cc1a2727843d
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-08-04 16:22:53 +0530

    feat: gets current trading stategy/changing current strategy and write logs

--- backend/accounts.py
+++ backend/accounts.py
@@ -105,3 +105,15 @@ class Account(BaseModel):
         data["total_profit_loss"] = pnl
         write_log(self.name, "account", "Retrieved account details")
         return json.dumps(data)
+
+    def get_strategy(self) -> str:
+        """ Return the strategy of the account """
+        write_log(self.name, "account", "Retrieved strategy")
+        return self.strategy
+
+    def change_strategy(self, strategy: str) -> str:
+        """ At your discretion, if you choose to, call this to change your investment strategy for the future """
+        self.strategy = strategy
+        self.save()
+        write_log(self.name, "account", "Changed strategy")
+        return "Changed strategy"
