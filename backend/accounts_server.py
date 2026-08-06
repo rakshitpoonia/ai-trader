fb2dae7f10fb845beb5000892c9bb6c1c75138e4
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2026-08-06 17:29:04 +0530

    feat: registered mcp resources to get trading report and startegy of an account

--- backend/accounts_server.py
+++ backend/accounts_server.py
@@ -61,5 +61,17 @@ async def change_strategy(name: str, strategy: str) -> str:
     return Account.get(name).change_strategy(strategy)
 
 
+@mcp.resource("accounts://accounts_server/{name}")
+async def read_account_resource(name: str) -> str:
+    account = Account.get(name.lower())
+    return account.report()
+
+
+@mcp.resource("accounts://strategy/{name}")
+async def read_strategy_resource(name: str) -> str:
+    account = Account.get(name.lower())
+    return account.get_strategy()
+
+
 if __name__ == "__main__":
     mcp.run(transport='stdio')
